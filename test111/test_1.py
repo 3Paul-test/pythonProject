a0c3b07dacccc91f627a9d95f95f382b80321eab
admin <1064194098@qq.com>
2024-07-08 15:53:04 +0800

    更新了

--- test111/test_1.py
+++ test111/test_1.py
@@ -1,4 +1,16 @@
 import requests
 
 
-print('测试github')
+def test_1(x,y):
+    if x>y:
+        print('x大于y')
+    elif x==y:
+        print('x=y')
+    else:
+        print('x小于y')
+
+
+
+
+
+test_1(10,5)
